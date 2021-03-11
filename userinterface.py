71292fc1535f9176fb780bdd035a06e8b03fc201
Tapedigo <Tapedigo1996@gmail.com>
2021-03-10 21:09:54 -0500

    added CurrencyConverter class

--- userinterface.py
+++ userinterface.py
@@ -1,16 +1,11 @@
-active = True
-while (active == True):
-    def WelcomeMessage():
-        print("Welcome to my currency conversion app!")
+def WelcomeMessage():
+    print("Welcome to my currency conversion app!")
 
-    def Input():
-        Usd = input('What amount would you like to convert(USD)? ')
-        print(Usd)
-        if Usd == int:
-            ValueError('Please enter an integer')
-            #break
-        else:
-            input("""What currency would you like to convert to?
-    1. GDP \n2. EUR \n""")
-
-    Input()
+def Input():
+    Usd = input('What amount would you like to convert(USD)? ')
+    print(Usd)
+    if Usd == int:
+        ValueError('Please enter an integer')
+    else:
+        input("""What currency would you like to convert to?
+1. GDP \n2. EUR \n""")
