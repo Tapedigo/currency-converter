2a4f6f9a24b25e97c06ec0c20bed2af3cb608a19
Tapedigo <Tapedigo1996@gmail.com>
2021-03-11 20:15:33 -0500

    Added while loop. I am currently unable to get it to break.

--- CurrencyAppCaller.py
+++ CurrencyAppCaller.py
@@ -1,23 +1,23 @@
 import urllib.request
 import json
-
 class CurrencyCoverter:
     def __init__(self, url):
         request = urllib.request.Request(url, headers={'User-Agent': 'Currency Converter'})
         data = urllib.request.urlopen(request).read()
         data = json.loads(data.decode('utf-8'))
         self.conversion_rates = data['conversion_rates']
-
+    
     def Convert(self, amount, from_money, to_money):
         original_amount = amount
         if from_money != 'USD':
             amount = amount / self.conversion_rates[from_money]
-        elif to_money == 'USD':
-            return original_amount, from_money, '=', amount, to_money
+        elif from_money == 'USD':
+            return str(original_amount) + " ("+ from_money + ") is equal to " + str(amount * self.conversion_rates[to_money]) + " ("+ to_money + ")"
         else:
-            return original_amount, from_money, '=', amount * self.conversion_rates[to_money], to_money
+            return original_amount, amount * self.conversion_rates[to_money]
+url = 'https://v6.exchangerate-api.com/v6/af370279909b3852fbc7252f/latest/USD'
+    
+converter = CurrencyCoverter(url)
 
-converter = CurrencyCoverter('https://v6.exchangerate-api.com/v6/af370279909b3852fbc7252f/latest/USD')
-
-print(converter.Convert(1.0, 'USD', 'AUD'))
+#print(converter.Convert(1.0, 'USD', 'AUD'))
 
--- userinterface.py
+++ userinterface.py
@@ -1,11 +1,21 @@
+import CurrencyAppCaller
+
+
 def WelcomeMessage():
     print("Welcome to my currency conversion app!")
 
 def Input():
     Usd = input('What amount would you like to convert(USD)? ')
-    print(Usd)
-    if Usd == int:
-        ValueError('Please enter an integer')
+    if Usd != float or int:
+        ValueError('Please enter an number')
     else:
-        input("""What currency would you like to convert to?
-1. GDP \n2. EUR \n""")
+        Currency = input("What currency would you like to convert to?")
+        converter = CurrencyAppCaller.CurrencyCoverter(url = 'https://v6.exchangerate-api.com/v6/af370279909b3852fbc7252f/latest/USD')
+        print(converter.Convert(Usd, 'USD', Currency.upper()))
+WelcomeMessage()
+
+while True:
+    UserInput = input
+    Input()
+    if input == 'quit'.lower :
+        break
