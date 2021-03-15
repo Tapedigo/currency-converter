b0400c26dca6af67198f5f72df8494798b9aedd0
Tapedigo <Tapedigo1996@gmail.com>
2021-03-14 20:27:51 -0400

    Got while loop and conversion from USD working.

--- CurrencyAppCaller.py
+++ CurrencyAppCaller.py
@@ -12,7 +12,7 @@ class CurrencyCoverter:
         if from_money != 'USD':
             amount = amount / self.conversion_rates[from_money]
         elif from_money == 'USD':
-            return str(original_amount) + " ("+ from_money + ") is equal to " + str(amount * self.conversion_rates[to_money]) + " ("+ to_money + ")"
+            return str(original_amount) + " ("+ from_money + ") is equal to " + (str(amount * self.conversion_rates[to_money])) + " ("+ to_money + ")"
         else:
             return original_amount, amount * self.conversion_rates[to_money]
 url = 'https://v6.exchangerate-api.com/v6/af370279909b3852fbc7252f/latest/USD'
--- userinterface.py
+++ userinterface.py
@@ -4,18 +4,22 @@ import CurrencyAppCaller
 def WelcomeMessage():
     print("Welcome to my currency conversion app!")
 
-def Input():
+def UserInput():
     Usd = input('What amount would you like to convert(USD)? ')
     if Usd != float or int:
         ValueError('Please enter an number')
-    else:
-        Currency = input("What currency would you like to convert to?")
-        converter = CurrencyAppCaller.CurrencyCoverter(url = 'https://v6.exchangerate-api.com/v6/af370279909b3852fbc7252f/latest/USD')
-        print(converter.Convert(Usd, 'USD', Currency.upper()))
+    
+
 WelcomeMessage()
 
 while True:
-    UserInput = input
-    Input()
-    if input == 'quit'.lower :
+    input_of_user = input("What amount would you like to convert(USD)? ")
+    
+    if input_of_user == 'quit'.lower():
         break
+    else:
+        Currency = input("What currency would you like to convert to? ")
+        converter = CurrencyAppCaller.CurrencyCoverter(url = 'https://v6.exchangerate-api.com/v6/af370279909b3852fbc7252f/latest/USD')
+        print(converter.Convert(float(input_of_user), 'USD', Currency.upper()))
+        continue
+    
