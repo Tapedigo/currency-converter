0642d7836e6b803fde9784c52ecf50d3f5b4ff12
Tapedigo <Tapedigo1996@gmail.com>
2021-04-01 22:12:16 -0400

    Made quality of life changes

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ This my code louisville project
 must have the following packages installed
 
 requests
+
+Call UserInterface.py
+
+When this application asks for a currency to convert to enter the three letter currency code in Uppercase ("CAD "AUD"  "EUR" ect.)
+
+This is a simple currency converter that:
+1. Implements a master loop were user can exit by entering the word “Quit” or "q" any case.
+2. Creates a list and populates with data, retrieves information, and then uses it in the program.
+3. Creates and calls methods.
+4. Connects to an external API and uses the information retrieved.
+5. Has a conversion tool that converts user input and displays it.
--- userinterface.py
+++ userinterface.py
@@ -15,11 +15,11 @@ WelcomeMessage()
 while True:
     input_of_user = input("What amount would you like to convert(USD)? ")
     
-    if input_of_user == 'quit'.lower():
+    if input_of_user == 'quit'.lower() or input_of_user == 'q'.lower():
         break
     else:
-        Currency = input("What currency would you like to convert to? ")
-        converter = CurrencyAppCaller.CurrencyCoverter(url = 'https://v6.exchangerate-api.com/v6/af370279909b3852fbc7252f/latest/USD')
+        Currency = input("What currency would you like to convert to? (\"USD\" \"AUD\" \"EUR\" etc.) ")
+        converter = CurrencyAppCaller.CurrencyCoverter('https://v6.exchangerate-api.com/v6/af370279909b3852fbc7252f/latest/USD')
         print(converter.Convert(float(input_of_user), 'USD', Currency.upper()))
         continue
     
